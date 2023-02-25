f4c29c1083b21b00e64ef5fa44829db0ae6d7408
belain3d <belain3d@gmail.com>
2023-02-25 18:00:33 +0100

    Updated main.asm

--- main.asm
+++ main.asm
@@ -14,25 +14,22 @@ jmp $
 printf:
 	pusha
 	str_loop:
-		mov al, [si]
+		mov al, [si]	
 		cmp al, 0
 		jne print_char
 		popa
-		ret		
+		ret
 
 	print_char:
 		mov ah, 0x0e
 		int 0x10
 		add si, 1
-		jmp str_loop	
+		jmp str_loop
+				
 
-STR_0: db "Hiya World!", 0x0a, 0x0d, 0
-STR_1: db "What you see here is a boot sector. And some text by me.", 0x0a, 0x0d, 0
-STR_2: db "Over for now!", 0
-
-
+STR_0: db "Loading in 16-bit Real Mode to memory location 0x7c00", 0x0a, 0x0d, 0
+STR_1: db "These messages use the BIOS interrupt 0x10 with the ah register set to 0x0e", 0x0a, 0x0d, 0
+STR_2: db "TrinityOS test booting was successful.", 0x0a, 0x0d, 0
 
 times 510-($-$$) db 0
 dw 0xAA55
-
-times 512 db 0
